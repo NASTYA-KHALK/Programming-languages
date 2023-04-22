684873a9162ae74a76ad2566a1fc4124a96207bc
Anastasiya <Anastasiya553@outlook.com>
2023-04-23 01:03:59 +0300

    Решила три задачи

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Enter first number:");
+// int numOne = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter second number:");
+// int numTwo = Convert.ToInt32(Console.ReadLine());
+
+// if (numOne < numTwo){
+//     Console.WriteLine($"Наименьшее число = {numOne}, а наибольшее число = {numTwo}");
+// }    
+// else{
+//     Console.WriteLine($"Наибольшее число = {numOne}, а наименьшее число = {numTwo}");
+// }
+
+// Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Enter first number:");
+// int numOne = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter second number:");
+// int numTwo = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter third number:");
+// int numThree = Convert.ToInt32(Console.ReadLine());
+// int max = numOne;
+
+// if(numOne > numTwo){
+//     max = numOne;
+// }
+// if(numTwo > numThree){
+//     max = numTwo;
+// }
+// if(numThree > numOne){
+//     max = numThree;
+// }
+// Console.WriteLine($"Максимальное число в списке = {max}");
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Enter number:");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num % 2 == 0){
+//     Console.WriteLine("Число четное");
+// }
+// else{
+//     Console.WriteLine("Число не является четным");
+// }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Enter number:");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = N % 2==0;
+
+while (count <= N)
+{
+        Console.Write(count+" ");
+        count++;
+}
